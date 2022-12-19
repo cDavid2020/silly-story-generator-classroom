c1040f830c255d931824306e583a1d497a51b580
Manav Misra <manavm@visionwebsoft.com>
2022-12-19 12:13:28 -0600

    Add utility functions
    Could be called 'lib' also.

---
+++ src/utils.js
@@ -0,0 +1,14 @@
+export const generateStory =
+
+// DESTRUCTURED PARAMETERS
+({person, place, situation, degrees, weight}) => {
+  return `It was 94 ${degrees} outside, so ${person} went for a walk. When they got to ${place}, they stared in horror for a few moments, then ${situation}. Bob saw the whole thing, but was not surprised — ${person} weighs 300 ${weight}, and it was a hot day.`
+}
+
+export const convertToKgs = (weight) => {
+  return weight * 0.453592
+}
+
+export const convertToCelsius = (degrees) => {
+  return (degrees - 32) * 5/9
+}
