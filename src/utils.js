323bbbe150b790f69b687e4a2e11cd0d908781cd
Manav Misra <manavm@visionwebsoft.com>
2022-12-20 09:41:59 -0600

    Add Story component
    Moves generateStory here.

--- src/utils.js
+++ src/utils.js
@@ -1,9 +1,3 @@
-export const generateStory =
-
-// DESTRUCTURED PARAMETERS
-({person, place, situation, degrees, weight}) => {
-  return `It was 94 ${degrees} outside, so ${person} went for a walk. When they got to ${place}, they stared in horror for a few moments, then ${situation}. Bob saw the whole thing, but was not surprised — ${person} weighs 300 ${weight}, and it was a hot day.`
-}
 
 export const convertToKgs = (weight) => {
   return weight * 0.453592
