2151c85842c48bd5f048da7c215fed9d8a5e56b0
Manav Misra <manavm@visionwebsoft.com>
2022-12-20 09:41:41 -0600

    Use `toLowerCase` for id and for

--- src/components/RadioInput.js
+++ src/components/RadioInput.js
@@ -1,3 +1,3 @@
 export default function RadioInput({label, name, checked}) {
-  return `<label for="${label}">${label}</label><input id="${label}" type="radio" name="${name}" value="us" ${checked ? "checked" : ""}>`
+  return `<label for="${label.toLowerCase()}">${label}</label><input id="${label.toLowerCase()}" type="radio" name="${name}" value="us" ${checked ? "checked" : ""}>`
 }
